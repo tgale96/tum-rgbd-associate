a5783622049136459155855cc4b54b90356ffebe
Trevor Gale <gale.t@husky.neu.edu>
2018-02-24 16:47:33 -0500

    partial commit: playing around with image compression to store data

---
+++ create_h5.py
@@ -0,0 +1,46 @@
+import argparse as ap
+import cv2
+import h5py as h5
+import numpy as np
+
+def stats(t):
+    t = np.array(t)
+    print("max, min, mean, std: {} / {} / {} / {}"
+          .format(t.max(), t.min(), np.mean(t), np.std(t)))
+    
+def read_images(dirname, fname, jpeg_compress=False):
+    with open(dirname + "/" + fname, 'r') as file:
+        for line in file:
+            # ignore comments
+            if line[0] == '#':
+                continue
+            img_name = line.split()[1]
+
+            img = cv2.imread(dirname + "/" + img_name, -1)
+
+            stats(img) # DEBUG
+            
+            if jpeg_compress:
+                img = cv2.imencode(".jpg", img, [cv2.IMWRITE_JPEG_QUALITY, 90])
+            else:
+                img = cv2.imencode(".png", img, [cv2.IMWRITE_PNG_COMPRESSION, 90])
+
+            # DEBUG
+            print(img)
+            img = cv2.imdecode(img, -1)
+            stats(img)
+
+            break
+
+    
+if __name__ == "__main__":
+    p = ap.ArgumentParser(description="""Insert a dataset of rgb, 
+    depth, and pose samples into the given hdf5 file""")
+
+    p.add_argument("--h5_file", help="hdf5 file name")
+    p.add_argument("--data_dir", help="directory containing data to add")
+    args = p.parse_args()
+
+    # Read in the rgb images
+    read_images(args.data_dir, "rgb.txt.assoc", jpeg_compress=True)
+    # read_images(args.data_dir, "depth.txt.assoc")
